53fa2c7b4f4f5881ffbf837394680b1c25681f75
Allan_Vinicius <allan.vralmeida@senacsp.edu.br>
2023-09-01 17:10:56 -0300

    Dia 01/09

--- models/Usuario.php
+++ models/Usuario.php
@@ -35,6 +35,31 @@ class Usuario{
 
    public function cadastrar($dados){
 
+      try{
+         $sql = "INSERT INTO {$this->table}(nome, email, senha, perfil)
+         VALUES(:nome, :email, :senha, :perfil)";
+
+
+         $stmt = $this->db->prepare($sql);
+      }catch(PDOException $e){
+         echo "Erro na preparação da consulta" .$e->getMessage();
+      }
+      $stmt = $this->db->prepare($sql);
+      
+      $stmt ->bindParam('nome', $dados['nome']);
+      $stmt ->bindParam('email', $dados['email']);
+      $stmt ->bindParam('senha', $dados['senha']);
+      $stmt ->bindParam('perfil', $dados['perfil']);
+
+      try{
+         $stmt->execute();
+         echo "Execução bem sucedida!";
+
+      }catch(PDOException $e){
+         echo "Erro na inserção dos dados" .$e->getMessage();
+      }
+     
+   
    }
 
    /**
@@ -46,8 +71,32 @@ class Usuario{
     */
    public function editar($id, $dados){
 
-   }
+      try{
+         $sql= ("UPDATE {$this->table} usuarios SET nome = :nome, email = :email, senha = :senha 
+         WHERE id_usuario = :$id");
 
+         $stmt = $this->db->prepare($sql);
+
+      }catch(PDOException $e){
+         echo "Erro na atualização dos dados: " . $e->getMessage();
+      }
+
+      $stmt ->bindParam('nome', $dados['nome']);
+      $stmt ->bindParam('email', $dados['email']);
+      $stmt ->bindParam('senha', $dados['senha']);
+      $stmt ->bindParam('perfil', $dados['perfil']);
+
+   try{
+
+      $stmt->execute();
+
+      echo "Seus dados foram atualizados com Sucesso!!";
+
+  } catch (PDOException $e) {
+
+      echo "erro na inserção: " . $e->getMessage();
+  }    
+   }
    //Excluir dados do usuário.
    public function excluir($id){
 
